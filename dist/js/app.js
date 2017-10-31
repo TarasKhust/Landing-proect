ae7d246fe65238b6fcde333d010c4dd4b9473fb3
TarasKhust <30997949+TarasKhust@users.noreply.github.com>
2017-10-31 18:14:32 +0200

    good work

--- dist/js/app.js
+++ dist/js/app.js
@@ -10388,11 +10388,32 @@ __webpack_require__(9)
 		var id = $(this).attr('href');
 		$(id).removeClass('hide');
 		$(this).parent().addClass('active');
-		return false
+		return fal
 	});
-	$('.banner-slider').slick({
+	$('.banner-slider, .testimonial-slider').slick({
+		arrows: false,
+        dots: true
+    });
+    $('.portfolio-slider').slick({
+        dots: true,
+        appendArrows: '.portfolio-slider_buttons',
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>'
     })
+
 });
+
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
 
 /* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(0)))
 
